65c6feacfdcf1246fd4bbd896189a119076bd2aa
Austin Brock <austinjbrock@gmail.com>
2022-06-16 20:53:09 -0700

    hooked up variables to dom and set url for API

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-console.log(fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0')
+let url = (fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0')
 .then(res=> res.json())
 .then(data=> console.log(data))
 )
 
 const weatherBtn = document.getElementById('get-weather-btn');
 
-weatherBtn.addEventListener('click', ()=>{alert('hello')})
+weatherBtn.addEventListener('click', ()=>{displayPara.textContent = url
+})
+
+let displayPara = document.getElementById('weather-details-para')
